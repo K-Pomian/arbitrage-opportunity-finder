dc2355ac7be386d100d0cba47cb3c8bd3717052c
K-Pomian <pomian.konrad@gmail.com>
2024-01-02 15:18:18 +0100

    Add ArbitrageFinder

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use structs::state::{State, STATE};
+use structs::{
+    arbitrage_finder::ArbitrageFinder,
+    state::{State, STATE},
+};
 
 mod config;
 mod structs;
@@ -26,8 +29,17 @@ async fn main() {
     });
 
     let state = STATE.get_or_init(|| async { State::new().await }).await;
+    let mut arbitrage_finder = ArbitrageFinder::new();
+
     loop {
-        println!("{:?}", state.get_latest_binance_ticker_data().read().await);
-        println!("{:?}", state.get_latest_pyth_price().read().await);
+        let maybe_opportunity = arbitrage_finder
+            .find_opportunity(
+                state.get_latest_pyth_price(),
+                state.get_latest_binance_ticker_data(),
+            )
+            .await;
+        if let Some(opportunity) = maybe_opportunity {
+            println!("Found an opportunity!\n{:#?}\n", opportunity);
+        }
     }
 }
--- src/structs/mod.rs
+++ src/structs/mod.rs
@@ -1,3 +1,4 @@
+pub mod arbitrage_finder;
 pub mod cex;
 pub mod on_chain;
 pub mod state;
