b0fc8a407af29a282bdd5e245acbd902ec7bbf5c
K-Pomian <pomian.konrad@gmail.com>
2024-01-02 16:48:31 +0100

    Add defaults to config

--- src/config.rs
+++ src/config.rs
@@ -6,13 +6,17 @@ pub static CONFIG: OnceCell<Config> = OnceCell::const_new();
 #[derive(Parser)]
 pub struct Config {
     // Pair from Binance spot market
-    #[arg(long, short)]
+    #[arg(long, short, default_value = "solusdt")]
     pub binance_ticker: String,
 
     // Price id pubkey from Pyth
     // List of available ids (Solana) can be found here:
     // https://pyth.network/price-feeds?cluster=solana-mainnet-beta
-    #[arg(long, short)]
+    #[arg(
+        long,
+        short,
+        default_value = "H6ARHf6YXhGYeQfUzQNGk6rDNnLBQKrenN712K4AQJEG"
+    )]
     pub pyth_price_id: String,
 }
 
